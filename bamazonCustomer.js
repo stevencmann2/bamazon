4b96288189f601441db5fe7980daad712ad1f2e9
Steven Mann <stevencmann2@gmail.com>
2019-12-11 11:57:22 -0600

    install table npm; displayed product info to user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@
 // console.log("Hello");
 const mysql = require("mysql");
 const inquirer = require('inquirer');
+//////FOR TABLE
+const {table} = require("table");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -14,11 +16,30 @@ const connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  console.log(connection);
-  console.log("connected as id " + connection.threadId);
-  connection.end();
-  // run the start function after the connection is made to prompt the user
+  // run the PRODUCT DISPLAY function after the connection is made to prompt the user
+  productDisplay();
 });
 
 
+////// PRODUCT DISPLAY FROM TABLE 
+function productDisplay(){
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    const data = res.map((products) => [products.item_id, products.product_name, products.department_name, products.price, products.stock_quantity]);
+    // console.log(data);
+    const productsTable = [
+      ['Item ID', 'Product Name', 'Department', 'Price', 'Left in Stock'], 
+      ...data
+    ];
+    ///////////////// UI DISPLAY/////////////////////////
+    console.log('\n\n');
+    console.log("-".repeat(122) + '\n');
+    console.log('Welcome to the Bamazon Store!' + '\n')
+    console.log("-".repeat(122) + '\n');
+    console.log('Here is what is in stock currently: ' + '\n')
+    console.log("-".repeat(122) + '\n');
+    console.log(table(productsTable));
+  });
+}
 
+////////// INQUIRER PROMPT 
